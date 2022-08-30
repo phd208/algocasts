c5399c7c88fd4374d3ad74e7e2978f162b7178ce
Pedro Dornas <65436615+phd208@users.noreply.github.com>
2022-08-30 12:55:46 -0700

    added exercises

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+ 
+  // return str.split('').reverse().join('');
+
+  let rev = '';
+
+  for (let i of str) {
+    rev = i + rev;
+  }
+
+  return rev;
+
+};
+
+module.exports = reverse;
